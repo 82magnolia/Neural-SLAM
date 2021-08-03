a78d3bbd389893564587842bac7dfb69cd4722cf
Eun Sun Lee <esun.k.lee@gmail.com>
2021-08-03 18:16:37 +0900

    added script for val

---
+++ scripts/val.sh
@@ -0,0 +1,4 @@
+python main.py --split val --eval 1 --train_global 0 --train_local 0 --train_slam 0 \
+--load_global pretrained_models/model_best.global \
+--load_local pretrained_models/model_best.local \
+--load_slam pretrained_models/model_best_noiseless_pretrained_old_torch.slam 
