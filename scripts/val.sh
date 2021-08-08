1b77e1d0712bff232f0a41f8a8809f4d6ba6d370
82magnolia <82magnolia@naver.com>
2021-08-08 20:45:11 +0900

    Fixed bug

--- scripts/val.sh
+++ scripts/val.sh
@@ -1,5 +1,3 @@
-tgt_slam=$1
-
 python main.py --split val --eval 1 --train_global 0 --train_local 0 --train_slam 0 \
 --load_global pretrained_models/model_best.global \
 --load_local pretrained_models/model_best.local \
