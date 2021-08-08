260fcb0b8768473efb213e5f65360ece1556db93
82magnolia <82magnolia@naver.com>
2021-08-08 15:42:48 +0000

    Fixed naming

--- scripts/gen_val_na_no_noiseless_actor_tta.sh
+++ scripts/gen_val_na_no_noiseless_actor_tta.sh
@@ -2,5 +2,5 @@ python main.py --video 1 --eval 1 --train_global 0 --train_local 0 --train_slam 
 --load_global pretrained_models/model_best.global \
 --load_local pretrained_models/model_best.local \
 --load_slam trained_models/model_best_noiseless_pretrained.slam --split val --auto_gpu_config 0 --num_processes 1 --noisy_actions 0 \
---exp_name val_na_no_noiseless_actor --noisy_actions 1 --noisy_odometry 1 --traj_per_scene 100
+--exp_name val_na_no_noiseless_actor_tta --noisy_actions 1 --noisy_odometry 1 --traj_per_scene 100
 
