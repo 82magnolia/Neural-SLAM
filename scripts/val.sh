a50fb72c0e38ae9f658471d519e050f5f1475ad0
82magnolia <82magnolia@naver.com>
2021-08-08 18:43:46 +0900

    Added easy running script

--- scripts/val.sh
+++ scripts/val.sh
@@ -1,8 +1,10 @@
+tgt_slam=$1
+
 python main.py --split val --eval 1 --train_global 0 --train_local 0 --train_slam 0 \
 --load_global pretrained_models/model_best.global \
 --load_local pretrained_models/model_best.local \
---load_slam pretrained_models/model_best_lr_00005.slam --num_episodes 2 \
---auto_gpu_config 0 -n1
+--load_slam "$1" --num_episodes 2 \
+--auto_gpu_config 0 -n1 --noisy_actions 1 --noisy_odometry 1
 
 
 
