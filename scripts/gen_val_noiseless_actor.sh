ba636b09eea11f48f310f56b21f4be124d2f682d
82magnolia <82magnolia@naver.com>
2021-08-08 18:40:29 +0900

    Added noiseless actor

--- scripts/gen_val_noiseless_actor.sh
+++ scripts/gen_val_noiseless_actor.sh
@@ -2,5 +2,5 @@ python main.py --video 1 --eval 1 --train_global 0 --train_local 0 --train_slam 
 --load_global pretrained_models/model_best.global \
 --load_local pretrained_models/model_best.local \
 --load_slam trained_models/model_best_noiseless_pretrained.slam --split val --auto_gpu_config 0 --num_processes 1 --noisy_actions 0 \
---exp_name val_na_no_noiseless_actor --noisy_actions 0 --noisy_odometry 0
+--exp_name val_noiseless_actor --noisy_actions 0 --noisy_odometry 0
 
